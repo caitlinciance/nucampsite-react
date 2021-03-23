dc6162a5ea9e62757aca4c329dca8584555dc59b
Caitlin Ciance <caitlinciance@gmail.com>
2021-03-23 14:20:20 -0400

    Redux Thunk

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import {
   Card,
   CardImg,
@@ -6,8 +6,9 @@ import {
   CardTitle,
   Breadcrumb,
   BreadcrumbItem,
-} from 'reactstrap'
-import { Link } from 'react-router-dom'
+} from "reactstrap";
+import { Link } from "react-router-dom";
+import { Loading } from "./LoadingComponent";
 
 function RenderDirectoryItem({ campsite }) {
   return (
@@ -19,18 +20,38 @@ function RenderDirectoryItem({ campsite }) {
         </CardImgOverlay>
       </Link>
     </Card>
-  )
+  );
 }
 
 function Directory(props) {
-  const directory = props.campsites.map((campsite) => {
+  const directory = props.campsites.campsites.map((campsite) => {
     return (
       <div key={campsite.id} className="col-md-5 m-1">
         <RenderDirectoryItem campsite={campsite} />
       </div>
-    )
-  })
+    );
+  });
 
+  if (props.campsites.isLoading) {
+    return (
+      <div className="container">
+        <div className="row">
+          <Loading />
+        </div>
+      </div>
+    );
+  }
+  if (props.campsites.errMess) {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h4>{props.campsites.errMess}</h4>
+          </div>
+        </div>
+      </div>
+    );
+  }
   return (
     <div className="container">
       <div className="row">
@@ -47,7 +68,7 @@ function Directory(props) {
       </div>
       <div className="row">{directory}</div>
     </div>
-  )
+  );
 }
 
-export default Directory
+export default Directory;
